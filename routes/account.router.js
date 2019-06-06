ed843cfc72096bbcef7bed194d7181585047fcf3
Quốc Hội <quocvu142@gmail.com>
2019-06-06 22:47:16 +0700

    cập nhật về tài khoản
    +Gửi mail quên mật khẩu(nodemailer)
    +Người dùng có HSD là 30 ngày

--- routes/account.router.js
+++ routes/account.router.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var userModel = require('../models/account/account.model')
 var bcrypt = require('bcrypt');
 var passport = require('passport');
+var nodemailer = require('nodemailer');
+var moment = require('moment');
+var randomstring = require("randomstring");
 
 router.get('/register', (req, res, next) => {
   res.render('account/register', {
@@ -23,7 +26,51 @@ router.get('/is-available', (req, res, next) => {
 })
 
 
-router
+
+router.get('/is-available2', (req, res, next) => {
+  var username = req.query.username;
+  userModel.single(username).then(rows => {
+    if (rows.length > 0) {
+      return res.json(true);
+    } else {
+      return res.json(false);
+    }
+  })
+})
+
+router.get('/forgot-password',(req,res,next)=>{
+  res.render('account/forgot_password',{
+    layout: 'account_layout'
+  });
+})
+
+router.post('/forgot-password',(req,res,next)=>{
+  var username= req.body.username;
+  var email = req.body.email;
+  userModel.single(username).then(rows=>{
+    var user = rows[0];
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+             user: 'quocvu142@gmail.com',
+             pass: 'quochoi42'
+         }
+     });
+     const mailOptions = {
+      from: 'quocvu142@gmail.com', // sender address
+      to: 'quochoi142@gmail.com', // list of receivers
+      subject: 'Forgot password', // Subject line
+      html: '<a href="http://localhost:3000/account/forgot-password"><b>Click here to reset password</b></a>'// plain text body
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info);
+   });
+  })
+})
 
 router.post('/register', (req, res, next) => {
   var entity = new Object;
@@ -37,8 +84,12 @@ router.post('/register', (req, res, next) => {
     entity.idChuyenMuc = req.body.category;
   } else {
     entity.idChuyenMuc = null;
+    var date = moment().add(30,'days').format('YYYY-MM-DD');
+    entity.HSD=date;
+
   }
   entity.daXoa = 0;
+  entity.code=randomstring.generate(15); 
   userModel.add(entity).then(n => {
     res.redirect('/admin/categories');
   }).catch(err => {
